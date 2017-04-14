a00aed61b4388a700d9c3c4c2fc728851b5dc3a8
ttomchy <ttomchy@163.com>
2017-04-14 20:43:47 +0800

    add delete.cpp to delete specific  dirctories

---
+++ examples/simple_nodes/delete.cpp
@@ -0,0 +1,79 @@
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include<iostream>
+#include <string.h>
+
+
+//判断是否为目录
+bool is_dir(const char *path)
+{
+    struct stat statbuf;
+    if(lstat(path, &statbuf) ==0)//lstat返回文件的信息，文件信息存放在stat结构中
+    {
+        return S_ISDIR(statbuf.st_mode) != 0;//S_ISDIR宏，判断文件类型是否为目录
+    }
+    return false;
+}
+
+//判断是否为常规文件
+bool is_file(const char *path)
+{
+    struct stat statbuf;
+    if(lstat(path, &statbuf) ==0)
+        return S_ISREG(statbuf.st_mode) != 0;//判断文件是否为常规文件
+    return false;
+}
+
+//判断是否是特殊目录
+bool is_special_dir(const char *path)
+{
+    return strcmp(path, ".") == 0 || strcmp(path, "..") == 0;
+}
+
+//生成完整的文件路径
+void get_file_path(const char *path, const char *file_name,  char *file_path)
+{
+    strcpy(file_path, path);
+    if(file_path[strlen(path) - 1] != '/')
+        strcat(file_path, "/");
+    strcat(file_path, file_name);
+}
+
+void delete_file(const char *path)
+{
+    DIR *dir;
+    dirent *dir_info;
+    char file_path[PATH_MAX];
+    if(is_file(path))
+    {
+        remove(path);
+        return;
+    }
+    if(is_dir(path))
+    {
+        if((dir = opendir(path)) == NULL)
+            return;
+        while((dir_info = readdir(dir)) != NULL)
+        {
+            get_file_path(path, dir_info->d_name, file_path);
+            if(is_special_dir(dir_info->d_name))
+                continue;
+            delete_file(file_path);
+            rmdir(file_path);
+        }
+    }
+    std::cout<<"Now it  is successfully delete all the files "<<std::endl;
+}
+int main(int argc, char **argv)
+{
+    std::cout<<"Now it is  starting to  delete all the files "<<std::endl;
+    delete_file("/home/hadoop/work_space/laser_ws/devel/lib/depth_clustering/result");
+
+    return 0;
+}
+
+
+
+
+
